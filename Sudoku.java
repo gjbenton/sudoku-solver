666ca84ee7c96bb9e03ed6be1c8ce1be67239433
gjbenton <gjbenton@dons.usfca.edu>
2020-09-07 11:38:33 -1000

    Debugged checkGrid() function. Now sudoku solver works as intended. Initial problem was all empty spaces returned empty after the recursive function.

--- Sudoku.java
+++ Sudoku.java
@@ -3,42 +3,55 @@ solution spaces on an existing sudoku board.
 */
 import java.util.Scanner;
 public class Sudoku{
-	private int[][] puzzle; 
+	private int[][] puzzle = {
+		{3, 0, 6, 5, 0, 8, 4, 0, 0},
+		{5, 2, 0, 0, 0, 0, 0, 0, 0},
+		{0, 8, 7, 0, 0, 0, 0, 3, 1},
+		{0, 0, 3, 0, 1, 0, 0, 8, 0}, 
+		{9, 0, 0, 8, 6, 3, 0, 0, 5},
+		{0, 5, 0, 0, 9, 0, 6, 0, 0}, 
+		{1, 3, 0, 0, 0, 0, 2, 5, 0}, 
+		{0, 0, 0, 0, 0, 0, 0, 7, 4}, 
+		{0, 0, 5, 2, 0, 6, 3, 0, 0} };
+; 
 	private final int EMPTY = 0;
 	private final int SIZE = 9;
 
 	public Sudoku(){
-		Scanner fill = new Scanner(System.in); // allow user to fill in solved solutions
-		//puzzle = new int[9][9]; //declares the 9x9 array 
-		puzzle = {{3, 0, 6, 5, 0, 8, 4, 0, 0},{5, 2, 0, 0, 0, 0, 0, 0, 0},{0, 8, 7, 0, 0, 0, 0, 3, 1}, {0, 0, 3, 0, 1, 0, 0, 8, 0}, {9, 0, 0, 8, 6, 3, 0, 0, 5},{0, 5, 0, 0, 9, 0, 6, 0, 0}, {1, 3, 0, 0, 0, 0, 2, 5, 0}, {0, 0, 0, 0, 0, 0, 0, 7, 4}, {0, 0, 5, 2, 0, 6, 3, 0, 0} };
+		// Scanner fill = new Scanner(System.in); // allow user to fill in solved solutions
+		// puzzle = new int[9][9]; //declares the 9x9 array 
 
 		// for (int i=0; i<puzzle.length;i++)
 		// 	for(int j=0; j<puzzle.length; j++){
 		// 		System.out.println("Enter value for [" +
 		// 			i +"]["+ j +"]. If solution is blank enter '0'");
 		// 		puzzle[i][j] = fill.nextInt();
-			//}
+		// }
 	}
-
 	public boolean fillBoard(){
 		//finds empty space
-		for(int row = 0; row<puzzle.length; row++)
+		for(int row = 0; row<puzzle.length; row++){
 			for(int col = 0; col<puzzle.length; col++){
 				if(puzzle[row][col] == EMPTY){
+					//System.out.print(puzzle[row][col]);
 					//now itereates through 1-9
+					
 					for(int num = 1; num <= SIZE; num++){
 						//validates num
 						if(checkRow(row, num) && checkCol(col, num) && checkGrid(row, col, num)){
 							puzzle[row][col]= num;
+							//System.out.print(puzzle[row][col]);
 							if (fillBoard())
 								return true;
-							else 
+							else
 								puzzle[row][col] = EMPTY;
+								//System.out.print(puzzle[row][col]);
 						}
 					}
 					return false;
 				}
 			}
+		}
 		return true;
 	}
 
@@ -63,8 +76,15 @@ public class Sudoku{
 		int gridCol = row/3;
 
 		//This iterative loop allows i to be 0/3/6 depending on gridRow
-		for(int i=gridRow*3; i<(gridRow*3)+3; i++){
-			for(int j= gridCol*3; j<(gridCol*3)+3;j++)
+		
+		// for(int i=gridRow*3; i<(gridRow*3)+3; i++){
+		// 	for(int j= gridCol*3; j<(gridCol*3)+3;j++)
+		// 		if(puzzle[i][j]==validate)
+		// 			return false;
+		// }
+
+		for(int i=gridCol*3; i<(gridCol*3)+3; i++){
+			for(int j= gridRow*3; j<(gridRow*3)+3;j++)
 				if(puzzle[i][j]==validate)
 					return false;
 		}
@@ -77,14 +97,15 @@ public class Sudoku{
 		for(int row =0; row<puzzle.length; row++){
 			output += "\n";
 			if(row%3==0)
-				output += "-----------------------------------------------\n";
+				output += "-------------------------------------------------------------------\n";
 			for(int col = 0; col<puzzle[row].length; col++){
 				if(col%3==0)
 					output += "| ";
 				output+= puzzle[row][col] + "\t";	
 			}
-
 		}
+		output += "\n-------------------------------------------------------------------";
+
 		return output;	
 	}
 
@@ -92,13 +113,13 @@ public class Sudoku{
 	public static void main(String[] args){
 		//instantiating the board allows the user to fill in spaces for sudoku board
 		Sudoku board = new Sudoku();
+		System.out.println("Unsolved Sudoku Board");
 		System.out.println(board);
 		
 		board.fillBoard();
+		System.out.println("Sudoku Puzzle Solved!");
 		System.out.println(board);
-
-		//System.out.println(board.emptyspace(0));
-		System.out.println("end.");
+		//System.out.println("end.");
 
 	}	
 }
